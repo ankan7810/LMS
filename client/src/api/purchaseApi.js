c3800b2609d113179dc6e11e4238def42d834616
ankan7810 <162693954+ankan7810@users.noreply.github.com>
2025-11-27 10:47:23 +0530

    Update purchaseApi.js

--- client/src/api/purchaseApi.js
+++ client/src/api/purchaseApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // const COURSE_PURCHASE_API = "https://lms-xrs4.onrender.com/api/v1/purchase";
-const COURSE_PURCHASE_API = "http://localhost:8000/api/v1/purchase";
+const COURSE_PURCHASE_API = "srv-d4jtru3e5dus73erng60/api/v1/purchase";
 
 export const purchaseApi = createApi({
   reducerPath: "purchaseApi",
