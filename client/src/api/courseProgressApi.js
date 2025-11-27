df92697eb8c0d433be5a054ccec3e68c2a5f4030
ankan7810 <162693954+ankan7810@users.noreply.github.com>
2025-11-27 10:58:44 +0530

    Update courseProgressApi.js

--- client/src/api/courseProgressApi.js
+++ client/src/api/courseProgressApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // const COURSE_PROGRESS_API = "https://lms-xrs4.onrender.com/api/v1/course-progress";
-const COURSE_PROGRESS_API = "srv-d4jtru3e5dus73erng60/api/v1/course-progress";
+const COURSE_PROGRESS_API = "https://lms-1-server.onrender.com/api/v1/course-progress";
 
 
 export const courseProgressApi = createApi({
